bf9e347668f3e334b851770f74aa89cfeed1829f
DeZoST <zosted62400@gmail.com>
2024-04-29 00:56:41 +0200

    Projects section faite, mais carousel fonctionne pas

--- src/main.js
+++ src/main.js
@@ -1,3 +1,8 @@
+const buttons = document.querySelectorAll('.arrow__btn');
+const slides = document.querySelectorAll('.slide');
+const carousel = document.querySelector(".projects__carousel");
+const slideWidth = 286;
+
 const observer = new IntersectionObserver((entries) => {
     entries.forEach((entry) => {
         if (entry.isIntersecting) {
@@ -8,3 +13,32 @@ const observer = new IntersectionObserver((entries) => {
 
 const hiddenElements = document.querySelectorAll('.hidden');
 hiddenElements.forEach((el) => observer.observe(el));
+
+let carouselOffset = 0;
+
+buttons.forEach((button) => {
+    button.addEventListener('click', (event) => {
+        const calcNextSlide = event.target.id === "next" ? 1 : -1;
+        const slideActive = document.querySelector('.active');
+        let newIndex = calcNextSlide + [...slides].indexOf(slideActive);
+        
+        carouselOffset += -calcNextSlide * slideWidth;
+        carousel.style.transform = `translateX(${carouselOffset}px)`;
+
+        // Vérifiez si l'index est en dehors des limites et ajustez-le si nécessaire
+        if (newIndex < 0) {
+            newIndex = slides.length - 1;
+            carouselOffset = -slideWidth * 2;
+            carousel.style.transform = `translateX(${carouselOffset}px)`;
+        } else if (newIndex >= slides.length) {
+            newIndex = 0;
+            carouselOffset = 0;
+            carouselOffset = slideWidth * 2;
+            carousel.style.transform = `translateX(${carouselOffset}px)`;
+        }
+        
+        slides[newIndex].classList.add('active');
+        slideActive.classList.remove('active');
+    });
+});
+
