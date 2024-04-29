7839c5f2035e7d8b70a656a44bcba95f05b3bfc6
DeZoST <zosted62400@gmail.com>
2024-04-29 07:08:37 +0200

    Ajouté le slider draggable pour téléphone

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ const carousel = document.querySelector(".projects__carousel");
 const container = document.querySelector(".projects__carousel-container");
 
 let currentItemIndex = 0;
-
+let startX = 0;
 
 function scrollToCurrentItem() {
   slides[currentItemIndex].scrollIntoView({ behavior: 'smooth', inline: 'center' });
@@ -36,6 +36,21 @@ function prevItem() {
 
 document.getElementById('next').addEventListener('click', nextItem);
 document.getElementById('prev').addEventListener('click', prevItem);
+container.addEventListener('touchstart', touchStart);
+container.addEventListener('touchmove', touchMove);
+
+function touchStart(e) {
+  startX = e.touches[0].clientX;
+}
+
+function touchMove(e) {
+  const diffX = e.touches[0].clientX - startX;
+  if (diffX > 0) {
+    prevItem();
+  } else if (diffX < 0) {
+    nextItem();
+  }
+}
 
 const observer = new IntersectionObserver((entries) => {
     entries.forEach((entry) => {
