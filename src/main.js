d450f360fd8739d7cfc2c951b1226caedac1fc53
DeZoST <zosted62400@gmail.com>
2024-04-29 07:19:50 +0200

    Projet fini

--- src/main.js
+++ src/main.js
@@ -4,7 +4,6 @@ const carousel = document.querySelector(".projects__carousel");
 const container = document.querySelector(".projects__carousel-container");
 
 let currentItemIndex = 0;
-let startX = 0;
 
 function scrollToCurrentItem() {
   slides[currentItemIndex].scrollIntoView({ behavior: 'smooth', inline: 'center' });
@@ -36,35 +35,6 @@ function prevItem() {
 
 document.getElementById('next').addEventListener('click', nextItem);
 document.getElementById('prev').addEventListener('click', prevItem);
-container.addEventListener('touchstart', touchStart);
-container.addEventListener('touchmove', touchMove);
-container.addEventListener('touchend', touchEnd);
-
-function touchStart(e) {
-  startX = e.touches[0].clientX;
-  isDragging = true;
-}
-
-function touchMove(e) {
-  if (!isDragging) return;
-
-  const diffX = e.touches[0].clientX - startX;
-  if (Math.abs(diffX) > 20) { // Threshold to prevent accidental touch events
-    e.preventDefault(); // Prevent default touch behavior like scrolling
-  }
-}
-
-function touchEnd(e) {
-  if (!isDragging) return;
-
-  const diffX = e.changedTouches[0].clientX - startX;
-  if (diffX > 20) {
-    prevItem();
-  } else if (diffX < -20) {
-    nextItem();
-  }
-  isDragging = false;
-}
 
 const observer = new IntersectionObserver((entries) => {
     entries.forEach((entry) => {
